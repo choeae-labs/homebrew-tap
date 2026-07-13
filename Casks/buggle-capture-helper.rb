31bb3baeaafaed17cce4c0e292319280bad39ff4
Jayoung <9ja9ja9ja@gmail.com>
2026-07-13 19:41:19 +0900

    chore: Cask url을 고정 파일명(BuggleCaptureHelper.dmg)에 맞춤

--- Casks/buggle-capture-helper.rb
+++ Casks/buggle-capture-helper.rb
@@ -2,7 +2,7 @@ cask "buggle-capture-helper" do
   version "0.1.3"
   sha256 :no_check # 릴리스 후 `shasum -a 256 <dmg>` 값으로 교체 권장(초기 임시)
 
-  url "https://github.com/choeae-labs/buggle-capture-helper/releases/download/v#{version}/BuggleCaptureHelper-#{version}-universal.dmg"
+  url "https://github.com/choeae-labs/buggle-capture-helper/releases/download/v#{version}/BuggleCaptureHelper.dmg"
   name "buggle 캡처 도우미"
   desc "buggle 전용 스크린샷·GIF 캡처 도우미"
   homepage "https://github.com/choeae-labs/buggle-capture-helper"
